5d67ebdd2b170eb5f6b061f33cab1efa57268683
Vishakha Gupta-Cledat <vishakha@aperturedata.io>
2024-10-04 08:26:30 +0000

    Add more quick start steps

---
+++ notebooks/load_cookbook_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p data; cd data
+
+# Get the script to generate the data.json
+wget https://github.com/aperture-data/Cookbook/raw/refs/heads/main/scripts/convert_ingredients_adb_csv.py
+
+# Run the script to generate the right CSV files
+python convert_ingredients_adb_csv.py
+
+adb ingest from-csv dishes.adb.csv --ingest-type IMAGE  --transformer common_properties --transformer image_properties --transformer clip_pytorch_embeddings
+adb ingest from-csv ingredients.adb.csv --ingest-type ENTITY
+adb ingest from-csv dish_ingredients.adb.csv --ingest-type CONNECTION
+
+cd -
