d3c27eafcf54003dd79a3bb16117e2f4b8b4dce2
Gautam Saluja <gautam@aperturedata.io>
2024-10-04 15:49:24 -0400

    Vector search example.

--- scripts/load_cookbook_data.sh
+++ scripts/load_cookbook_data.sh
@@ -8,6 +8,9 @@ wget https://github.com/aperture-data/Cookbook/raw/refs/heads/main/scripts/conve
 # Run the script to generate the right CSV files
 python convert_ingredients_adb_csv.py
 
+# OpenAI CLIP model for generating embeddings.
+pip install git+https://github.com/openai/CLIP.git
+
 adb ingest from-csv dishes.adb.csv --ingest-type IMAGE  --transformer common_properties --transformer image_properties --transformer clip_pytorch_embeddings
 adb ingest from-csv ingredients.adb.csv --ingest-type ENTITY
 adb ingest from-csv dish_ingredients.adb.csv --ingest-type CONNECTION
